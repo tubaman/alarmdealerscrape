71e76228411590e517a8ccd36da0b98c239b9005
Ryan Nowakowski <tubaman@fattuba.com>
2017-07-09 23:22:15 -0500

    test scripts

---
+++ getevents.py
@@ -0,0 +1,32 @@
+import sys
+import netrc
+import csv
+
+from alarmdealerscrape import AlarmDealerClient
+
+
+def get_credentials():
+    auth = netrc.netrc().authenticators(AlarmDealerClient.DOMAIN)
+    username, account, password = auth
+    return username, password
+
+
+def output_as_csv(events):
+    writer = csv.writer(sys.stdout)
+    writer.writerow(AlarmDealerClient.EVENT_LOG_HEADERS)
+    writer.writerows(events)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    username, password = get_credentials()
+    client = AlarmDealerClient()
+    client.login(username, password)
+    events = client.get_event_log()
+    output_as_csv(events)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ test_arm_disarm.py
@@ -0,0 +1,46 @@
+import sys
+import netrc
+import time
+
+from alarmdealerscrape import AlarmDealerClient
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    auth = netrc.netrc().authenticators(AlarmDealerClient.DOMAIN)
+    username, code, password = auth
+
+    client = AlarmDealerClient()
+
+    print "logging in"
+    client.login(username, password)
+
+    print "waiting until ready to arm"
+    client.wait_for_status("System is Ready to Arm")
+
+    print "arming"
+    client.arm_stay()
+
+    print "waiting until exit delay"
+    client.wait_for_status("Exit Delay in Progress")
+
+    print "waiting 5 secs"
+    time.sleep(5)
+
+    #print "waiting until system armed"
+    #client.wait_for_status("System Armed")
+
+    print "disarming"
+    client.disarm(code)
+
+    #print "waiting until system disarmed"
+    #client.wait_for_status("System Disarmed")
+
+    print "waiting until ready to arm"
+    client.wait_for_status("System is Ready to Arm")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
