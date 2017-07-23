2863bcf68cf2bb6db53bb3905dcd691c9519d5be
Ryan Nowakowski <tubaman@fattuba.com>
2017-07-23 17:49:48 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+long_desc = open('README.md').read()
+
+setup(name='alarmdealerscrape',
+    version='1.0',
+    description='scraper for alarmdealer.com',
+    long_description=long_desc,
+    py_modules=('alarmdealerscrape',),
+    install_requires=[
+        'beautifulsoup4',
+        'requests>=2.2',
+        'websocket-client>=0.44',
+    ],
+    license='BSD',
+    author='Ryan Nowakowski',
+    author_email='tubaman@fattuba.com',
+    url='https://github.com/tubaman/alarmdealerscrape',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries',
+    )
+)
